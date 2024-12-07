affe20d8d2c186eb1a64816d61e5538a31c0a0e5
weiw3127 <bwlw3127@gmail.com>
2024-12-07 15:59:23 +1100

    eccomerce + chart

--- src/pages/Charts/Stacked.jsx
+++ src/pages/Charts/Stacked.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, StackingColumnSeries, Tooltip } from '@syncfusion/ej2-react-charts';
+import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis } from '../../data/dummy';
 
-const Stacked = () => {
+
+
+const Stacked = ({width, height}) => {
   return (
-    <div>Stacked</div>
+    <ChartComponent
+      width={width}
+      height={height}
+      id="Stack Chart"
+      primaryXAxis={stackedPrimaryXAxis}
+      primaryYAxis={stackedPrimaryYAxis}
+    >
+      <Inject services={[StackingColumnSeries, Category, Legend, Tooltip]} />
+    </ChartComponent>
   )
 }
 
